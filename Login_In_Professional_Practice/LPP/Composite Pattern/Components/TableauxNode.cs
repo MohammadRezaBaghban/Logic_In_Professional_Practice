18e7faa1e1f0eade1ce972e70ed53446ffd1b852
Mrbh <Mrbhmr@gmail.com>
2020-06-22 11:27:29 +0200

    Refactor IsClosed() Method and extract FindContradiction() Method

--- Login_In_Professional_Practice/LPP/Composite Pattern/Components/TableauxNode.cs
+++ Login_In_Professional_Practice/LPP/Composite Pattern/Components/TableauxNode.cs
@@ -6,7 +6,7 @@ namespace LPP.Composite_Pattern.Components
     public class TableauxNode
     {
         //Fields
-        public List<Component> Components;
+        public readonly List<Component> Components;
         public TableauxNode ParentNode;
         public TableauxNode LeftNode;
         public TableauxNode RightNode;
@@ -29,7 +29,7 @@ namespace LPP.Composite_Pattern.Components
                 parent.LeftNode ??= this;
         }
         public TableauxNode(List<Component> components, Component processed, TableauxNode parent)
-        {
+        { // Used for non-branched a-rules
             this.Components = new List<Component>();
             this.ParentNode = parent;
             parent.Branched = false;
@@ -50,8 +50,8 @@ namespace LPP.Composite_Pattern.Components
                 }
             });
         }
-        public TableauxNode(Component node, Component processed, TableauxNode parent)
-        {
+        public TableauxNode(Component node, Component processed, TableauxNode parent,bool branched)
+        { // Used for branched B-rules
             this.Components = new List<Component>();
             node.Belongs = this;
             Components.Add(node);
@@ -64,13 +64,50 @@ namespace LPP.Composite_Pattern.Components
                     this.Components.Add(newNode);
                 }
             });
-            parent.Branched = true;
+            parent.Branched = branched;
             this.ParentNode = parent;
             if (parent.LeftNode == null) parent.LeftNode = this;
-            else if (parent.RightNode == null) parent.RightNode = this;
+            else if (parent.RightNode == null && branched == true) parent.RightNode = this;
 
         }
 
+        public void IsClosed()
+        {
+            if (Closed != true)
+            {
+                this.Components.ForEach(x => InfixFormulaGenerator.Calculator.Calculate(x));
+                if (this.LeftNode == null && this.RightNode == null)
+                {// Node has not being Processed
+                    if (Components.Count > 1)
+                    {
+                        FindContradiction();
+                        if (Branched == false && LeftNode == null)
+                        {
+                            this.Closed = false;
+                        }
+                        else
+                        {
+                            if (this.Closed != true)
+                            {
+                                this.Evaluate();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //Root of Tableaux Node
+                        this.Evaluate();
+                    }
+                }
+                else
+                {
+                    //Node Has already Processed and simplified
+                    this.Closed = Branched == false
+                        ? LeftNode.Closed
+                        : LeftNode.Closed == true && this.RightNode.Closed == true;
+                }
+            }
+        }
         private void Evaluate()
         {
             if (Closed == null)
@@ -88,89 +125,33 @@ namespace LPP.Composite_Pattern.Components
                 IsClosed();
             }
         }
-
-        public void IsClosed()
+        private void FindContradiction()
         {
-            if (Closed != true)
+            for (var i = 0; i < Components.Count && this.Closed != true; i++)
             {
-                this.Components.ForEach(x => InfixFormulaGenerator.Calculator.Calculate(x));
-                if (this.LeftNode == null && this.RightNode == null)
-                {// Node has not being Processed
-                    if (Components.Count > 1)
-                    {
-                        if (Branched == false && LeftNode == null)
-                        {
-                            this.Closed = false;
-                        }
-                        else
-                        {
-                            for (var i = 0; i < Components.Count && this.Closed != true; i++)
-                            {
-                                for (var j = i + 1; j < Components.Count; j++)
-                                {
-                                    var node1Formula = Components[i].InFixFormula;
-                                    var node2Formula = Components[j].InFixFormula;
+                for (var j = i + 1; j < Components.Count; j++)
+                {
+                    var node1Formula = Components[i].InFixFormula;
+                    var node2Formula = Components[j].InFixFormula;
 
-                                    if (Components[j] is SingleComponent)
-                                    {
-                                        node2Formula = Components[j] is SingleComponent ? $"¬{node2Formula}" : $"¬({node2Formula})";
-                                        if (node1Formula != node2Formula) continue;
-                                        this.Closed = true;
-                                        break;
-                                    } else if (Components[i] is SingleComponent)
-                                    {
-                                        node1Formula = Components[i] is SingleComponent ? $"¬{node1Formula}" : $"¬({node1Formula})";
-                                        if (node1Formula != node2Formula) continue;
-                                        this.Closed = true;
-                                        break;
-                                    }
-                                    
-                                }
-                            }
-                            if (this.Closed != true)
-                            {
-                                if (!Components.Exists(x => x is CompositeComponent))
-                                {
-                                    this.Closed = false;
-                                }
-                                else
-                                {
-                                    this.Evaluate();
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {//Root of Tableaux Node
-                        this.Evaluate();
-                    }
-                }
-                else
-                {//Node Has already Processed and simplified
-                    if (Branched == false)
+                    if (Components[j] is SingleComponent)
                     {
-                        if (this.LeftNode.Closed == true)
-                            this.Closed = true;
-                        else if (this.LeftNode.Closed == false)
-                            this.Closed = false;
+                        node2Formula = Components[j] is SingleComponent ? $"¬{node2Formula}" : $"¬({node2Formula})";
+                        if (node1Formula != node2Formula) continue;
+                        this.Closed = true;
+                        break;
                     }
-                    else
+                    else if (Components[i] is SingleComponent)
                     {
-                        if (this.LeftNode.Closed == true && this.RightNode.Closed == true)
-                            this.Closed = true;
-                        else if (this.LeftNode.Closed == false || this.RightNode.Closed == false)
-                            this.Closed = false;
-                        else
-                        {
-                            this.LeftNode.Evaluate();
-                            this.RightNode.Evaluate();
-                        }
+                        node1Formula = Components[i] is SingleComponent ? $"¬{node1Formula}" : $"¬({node1Formula})";
+                        if (node1Formula != node2Formula) continue;
+                        this.Closed = true;
+                        break;
                     }
                 }
             }
-            
         }
-
+        
         public string Label()
         {
             var label = "";
